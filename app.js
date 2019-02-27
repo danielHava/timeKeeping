42e391ddd511b8a1fd90bdb98b3c2b2885515abe
Daniel Hava <dani.hava@protonmail.com>
2019-02-27 16:45:36 +0200

    attached swagger documentation

--- app.js
+++ app.js
@@ -6,14 +6,24 @@ const todosRoute = require('./routes').todosRoute;
 const usersRoute = require('./routes').usersRoute;
 const authRoute = require('./routes').authRoute;
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+const options = {
+    // explorer : false,
+    // customCss: '.swagger-ui .topbar { display: none }',
+    title: "da"
+  };
+ 
 const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/todos/', todosRoute);
-app.use('/api/users/', usersRoute);
-app.use('/api/auth', authRoute);
+app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
+
+app.use('/api/v1/todos/', todosRoute);
+app.use('/api/v1/users/', usersRoute);
+app.use('/api/v1/auth', authRoute);
 
 module.exports = app;
